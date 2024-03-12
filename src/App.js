c19d02314b6cfc44f3fd66150a24a207614ca5d0
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-03-12 14:26:30 -0400

    Button Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App(){
-    return <div>
-        APP 2
-    </div>
-}
+import Button from "./Button";
 
+function App() {
+  return (
+    <div>
+      <div ><Button>Click Here !</Button></div>
+      <div><Button>Buy Now</Button></div>
+      <div><Button>Exit</Button></div>
+      <div><Button>Hide ads</Button></div>
+      <div><Button>Save</Button></div>
+    </div>
+  );
+}
 
 export default App;
