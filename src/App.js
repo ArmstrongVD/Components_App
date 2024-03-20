b4f309ed455aefda451c29b6c6fd2ee76a98fd00
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-03-20 17:32:06 -0400

    Buttons Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
-      <div ><Button>Click Here !</Button></div>
-      <div><Button>Buy Now</Button></div>
-      <div><Button>Exit</Button></div>
-      <div><Button>Hide ads</Button></div>
-      <div><Button>Save</Button></div>
+      <div>
+        <Button success rounded className="mb-5">
+          {" "}
+          <GoBell />
+          Click Here !{" "}
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoCloudDownload />
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>Exit</Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoDatabase />
+          Hide ads
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Save
+        </Button>
+      </div>
     </div>
   );
 }
