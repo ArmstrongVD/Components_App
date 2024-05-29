b5626f4fad3ce7e3e5480d11b6c8d240fc4a8d3b
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-05-29 07:41:43 -0400

    Update to components

---
+++ src/components/Table.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Table({ data }) {
+    
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruits</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
+}
